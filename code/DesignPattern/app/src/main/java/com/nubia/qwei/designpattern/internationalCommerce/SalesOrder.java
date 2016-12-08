29e0eabb5f84902fa1905db3530b110bd18e857b
qwei <wei.qiang6@zte.com.cn>
2016-12-08 09:02:28 +0800

    问题描述: 主要是新增UML和code目录
    修改类型:
    问题原因:
    解决方法:
    PR号:
    TFS_ID:
    关联测试场景路径:
    用例编号:
    评审人:

---
+++ code/DesignPattern/app/src/main/java/com/nubia/qwei/designpattern/internationalCommerce/SalesOrder.java
@@ -0,0 +1,17 @@
+package com.nubia.qwei.designpattern.internationalCommerce;
+
+/**
+ * Author qwei
+ * Date 2016-12-7
+ * Company NUBIA TECHNOLOGY CO..LTD
+ */
+
+public class SalesOrder {
+
+
+    public int calcTax(){
+
+        return 0;
+    }
+
+}
