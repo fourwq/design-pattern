bc1d5624efcbeb39c35868a063a4d16140013852
qwei <wei.qiang6@zte.com.cn>
2016-12-08 15:47:04 +0800

    问题描述: 新增Strategy模式介绍

--- code/DesignPattern/app/src/main/java/com/nubia/qwei/designpattern/internationalCommerce/SalesOrder.java
+++ code/DesignPattern/app/src/main/java/com/nubia/qwei/designpattern/internationalCommerce/SalesOrder.java
@@ -8,10 +8,64 @@ package com.nubia.qwei.designpattern.internationalCommerce;
 
 public class SalesOrder {
 
+    private static final int CANAD = 1;
+    private static final int USA = 2;
 
-    public int calcTax(){
 
+    public int calcTax(int myNation){
+        switch(myNation){
+            case USA:
+                //US tax rule
+                break;
+            case CANAD:
+                //Canada tax rule
+                break;
+        }
         return 0;
     }
+
+
+    public int calcCurrency(int myNation){
+        switch(myNation){
+            case USA:
+                //US Currency rule
+                break;
+            case CANAD:
+                //Canada Currency rule
+                break;
+        }
+        return 0;
+    }
+
+    public int getDate(int myNation){
+            switch(myNation){
+            case USA:
+                //US mm/dd/yy
+                break;
+            case CANAD:
+                //Canada dd/mm/yy
+                break;
+        }
+        return 0;
+    }
+
+
+    public int getLanguage(int myNation){
+        boolean inQuebec=false;
+        switch(myNation){
+            case USA:
+                // use english
+                break;
+            case CANAD:
+                if(inQuebec){
+                    //use french
+                }else{
+                    //use english
+                }
+                break;
+        }
+        return 0;
+    }
+
 
 }
